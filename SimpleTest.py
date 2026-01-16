0648a0aafedac6cb9a3dd1bd22e618c622991fa8
Fabien Bastide <fabien.bastide@ansys.com>
2026-01-16 11:58:46 +0100

    Modified in Eng

--- SimpleTest.py
+++ SimpleTest.py
@@ -6,8 +6,8 @@ root.minsize(200, 200)
 root.maxsize(500, 500)
 root.geometry("300x300+50+50")
 
-# Ajout d'un bouton avec un fond rouge
+# Add red button
 bouton = tk.Button(root, text="Cliquez ici", background="red", fg="white")
-bouton.pack(pady=20)  # Affiche le bouton avec un espace vertical
+bouton.pack(pady=20)  # Display vertical
 
 root.mainloop()

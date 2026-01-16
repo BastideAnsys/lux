661b447f7c9f9270bba658eb59e3b0b424328e14
Fabien Bastide <fabien.bastide@ansys.com>
2026-01-16 11:48:55 +0100

    Added a color red background

--- SimpleTest.py
+++ SimpleTest.py
@@ -6,4 +6,8 @@ root.minsize(200, 200)
 root.maxsize(500, 500)
 root.geometry("300x300+50+50")
 
+# Ajout d'un bouton avec un fond rouge
+bouton = tk.Button(root, text="Cliquez ici", background="red", fg="white")
+bouton.pack(pady=20)  # Affiche le bouton avec un espace vertical
+
 root.mainloop()
